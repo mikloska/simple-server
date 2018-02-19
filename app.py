b39a29d7fa69824a6f8eef9b4864b69a5ce2ea9e
Luca <mikloska973@gmail.com>
2018-02-19 17:47:05 -0500

    Added extra lines to not get error if name is not provided.

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ def yo(name):
     return render_template('page.html',name=name)
 def index():
     return render_template('index.html')
+@app.route('/yo')
+def page():
+    return render_template('page.html')
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+# site is http://127.0.0.1:5000
